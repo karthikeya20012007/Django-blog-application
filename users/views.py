e2d249555adf1865618cb5190debc3570b221781
Karthikeya <karthikeya20012007@gmail.com>
2026-01-31 19:45:06 +0530

    added protected views

--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+
+
 
 def register(request):
     if request.method == 'POST':
@@ -19,5 +22,7 @@ def logout_view(request):
     logout(request)
     return render(request, 'users/logout.html')
 
+
+@login_required
 def profile(request):
     return render(request, 'users/profile.html') 
