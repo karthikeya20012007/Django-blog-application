65f160b5598702d20777cd053afe8e38c5642246
Karthikeya <karthikeya20012007@gmail.com>
2026-02-01 02:06:40 +0530

    add profile and profile picture

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth import views as auth_views
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('register/',user_views.register,name='register'),
     path('profile/',user_views.profile,name='profile'),
@@ -26,3 +29,6 @@ urlpatterns = [
     path('', include('blog.urls')),
     path("logout/", user_views.logout_view, name='logout'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
