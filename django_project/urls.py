daf346de4f550846b4771aac8f4e7cdc258c62a5
Karthikeya <karthikeya20012007@gmail.com>
2026-01-31 16:05:49 +0530

    added login and logout features

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
 from users import views as user_views
 urlpatterns = [
     path('register/',user_views.register,name="register"),
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name="login"),
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path("logout/", user_views.logout_view, name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -8,8 +8,13 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account Created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your Account Has Been Created! You can Login now')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})
+
+from django.contrib.auth import logout
+def logout_view(request):
+    logout(request)
+    return render(request, 'users/logout.html')
