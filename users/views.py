8554ecb301b5bf4a6b6e8cda46e29d02345ba980
Karthikeya <karthikeya20012007@gmail.com>
2026-02-01 15:36:44 +0530

    added profile update for users

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -25,4 +25,27 @@ def logout_view(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html') 
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+        
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+        
+        messages.success(request, f'Your Profile has Been Updated')
+        return redirect('profile')
+        #return render(request, 'users/profile.html') 
+        # if we use render the website asks for 'confirm resubmission' every time we reload the page, hence we are using redirect
+        # redirect uses get request, hence it loads without asking for resubmission.
+        
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+        
+            
+    context = {
+        'u_form':u_form,
+        'p_form':p_form       
+        }
+    return render(request, 'users/profile.html',context) 
