0ef6eec269eb146972a08afb03b672f0e524dbc8
Karthikeya <karthikeya20012007@gmail.com>
2026-02-01 16:19:45 +0530

    create a profile for all users to avoid profile not found error

--- users/signals.py
+++ users/signals.py
@@ -8,6 +8,8 @@ def create_profile(sender,instance,created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
         
-@receiver(post_save,sender=User)
+
+@receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    if hasattr(instance, 'profile'):
+        instance.profile.save()
